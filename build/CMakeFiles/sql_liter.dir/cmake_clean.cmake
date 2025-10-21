dae9150b4144d67990ecb2c2fe21fe69ba92f6b2
hobbbbies <stefankvitanov@gmail.com>
2025-10-21 16:34:45 -0400

    Modulized project with windusrf

---
+++ build/CMakeFiles/sql_liter.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sql_liter.dir/src/input_buffer.cpp.o"
+  "CMakeFiles/sql_liter.dir/src/input_buffer.cpp.o.d"
+  "CMakeFiles/sql_liter.dir/src/main.cpp.o"
+  "CMakeFiles/sql_liter.dir/src/main.cpp.o.d"
+  "CMakeFiles/sql_liter.dir/src/meta_command_processor.cpp.o"
+  "CMakeFiles/sql_liter.dir/src/meta_command_processor.cpp.o.d"
+  "CMakeFiles/sql_liter.dir/src/row.cpp.o"
+  "CMakeFiles/sql_liter.dir/src/row.cpp.o.d"
+  "CMakeFiles/sql_liter.dir/src/statement_processor.cpp.o"
+  "CMakeFiles/sql_liter.dir/src/statement_processor.cpp.o.d"
+  "CMakeFiles/sql_liter.dir/src/table.cpp.o"
+  "CMakeFiles/sql_liter.dir/src/table.cpp.o.d"
+  "CMakeFiles/sql_liter.dir/src/tokenizer.cpp.o"
+  "CMakeFiles/sql_liter.dir/src/tokenizer.cpp.o.d"
+  "CMakeFiles/sql_liter.dir/src/utils.cpp.o"
+  "CMakeFiles/sql_liter.dir/src/utils.cpp.o.d"
+  "sql_liter"
+  "sql_liter.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sql_liter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
