ba78e9d946e39b1ce26b6ff6b257cbba7ddcad37
hobbbbies <stefankvitanov@gmail.com>
2025-11-06 09:41:20 -0500

    deleted build directory for repo

--- build/_deps/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/stefanvitanov/repos/Personal Projects/cpp/sql_liter/build/_deps/googletest-src/googletest
-# Build directory: /Users/stefanvitanov/repos/Personal Projects/cpp/sql_liter/build/_deps/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
