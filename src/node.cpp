7bdc800e63fda0b4cd274e2e5e7c2c007cbb4f60
hobbbbies <stefankvitanov@gmail.com>
2025-11-08 17:47:33 -0500

    Adding leafNodeInsert

--- src/node.cpp
+++ src/node.cpp
@@ -19,3 +19,24 @@ void* Node::leafNodeValue(uint32_t cellNum) {
 void Node::initializeLeafNode() {
     *leafNodeNumCells() = 0;
 }
+
+void Node::leafNodeInsert(uint32_t key, Row* value, uint32_t cellNum) {
+    
+    // split here 
+    if (cellNum >= LEAF_NODE_MAX_CELLS) {
+        throw std::out_of_range("Cell number exceeds maximum cells");
+    }
+
+    uint32_t numCells = *leafNodeNumCells();
+
+    if (cellNum < numCells) {
+        for(uint32_t i = numCells; i > cellNum; i--) {
+            std::memcpy(leafNodeCell(i), leafNodeCell(i-1), LEAF_NODE_CELL_SIZE);
+        }
+    }
+    
+    uint8_t* cell = leafNodeCell(cellNum);
+    *leafNodeKey(cellNum) = key;
+    std::memcpy(leafNodeValue(cellNum), value, ROW_SIZE);
+    *leafNodeNumCells() = numCells + 1;
+}
